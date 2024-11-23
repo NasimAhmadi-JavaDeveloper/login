3ebaf11c5cfef7a66a6e8dd5f618b0b8a25f7f23
Nasim Ahmadi Ramezanlou <nasim.ahmadi@rasha.io>
2024-11-23 13:31:36 +0330

    commit yml

--- src/main/java/com/example/login/repository/UserRepository.java
+++ src/main/java/com/example/login/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.example.login.repository;
 
 import com.example.login.model.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,4 +12,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUserName(String userName);
 
     Optional<User> findByEmail(String email);
+
+    @EntityGraph(attributePaths = "posts")
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    User findUserWithPosts(@Param("id") Long id);
 }
