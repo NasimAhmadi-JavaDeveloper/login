a9c86d92591f40847760523ad410b5ee545f0330
Mohsen Mesrzadeh <mohsenmesr@yahoo.com>
2026-06-14 17:18:14 +0330

    optimus api

--- src/main/java/ir/tamin/teco/application/result/ServiceTokenResult.java
+++ src/main/java/ir/tamin/teco/application/result/ServiceTokenResult.java
@@ -1,16 +1,5 @@
 package ir.tamin.teco.application.result;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-public class ServiceTokenResult {
-
-    private String token;
+public record ServiceTokenResult(String token) {
 
 }
