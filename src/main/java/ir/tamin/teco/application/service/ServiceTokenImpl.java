4175533416291a69c0e2212021beb3ca1a32abc6
Mohsen Mesrzadeh <mohsenmesr@yahoo.com>
2026-06-15 10:45:20 +0330

    final interfaces

--- src/main/java/ir/tamin/teco/application/service/ServiceTokenImpl.java
+++ src/main/java/ir/tamin/teco/application/service/ServiceTokenImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ServiceTokenHandler implements ServiceTokenService {
+public class ServiceTokenImpl implements ServiceTokenService {
 
   private final OptimusOut optimusOut;
 
